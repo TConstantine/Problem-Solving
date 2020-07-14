6244ecf2ca0a72e966f8d20e86c5de147e5f8f17
Constantine Theodoridis <theodoridisconstantine@gmail.com>
2020-07-14 16:17:43 +0300

    Percolation should throw exception when open is called with invalid argument

--- src/main/java/constantine/theodoridis/problemsolving/percolation/Percolation.java
+++ src/main/java/constantine/theodoridis/problemsolving/percolation/Percolation.java
@@ -2,8 +2,17 @@ package constantine.theodoridis.problemsolving.percolation;
 
 public class Percolation {
 
+  private final int size;
+
   public Percolation(int size) {
     if (size <= 0) {
+      throw new IllegalArgumentException();
+    }
+    this.size = size;
+  }
+
+  public void open(int row, int column) {
+    if (row <= 0 || row > size || column <= 0 || column > size) {
       throw new IllegalArgumentException();
     }
   }
--- src/test/java/constantine/theodoridis/problemsolving/percolation/PercolationCorrectnessTest.java
+++ src/test/java/constantine/theodoridis/problemsolving/percolation/PercolationCorrectnessTest.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(JUnitParamsRunner.class)
 public class PercolationCorrectnessTest {
 
+  private Percolation percolation;
+
   @Test(expected = IllegalArgumentException.class)
   @Parameters({
     "-10",
@@ -15,7 +17,24 @@ public class PercolationCorrectnessTest {
     "0"
   })
   public void shouldThrowException_WhenConstructorIsCalledWithInvalidArgument(int size) {
-    final Percolation percolation = new Percolation(size);
+    percolation = new Percolation(size);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  @Parameters({
+    "10, -1, 5",
+    "10, 11, 5",
+    "10, 0, 5",
+    "10, 5, -1",
+    "10, 5, 11",
+    "10, 5, 0",
+    "10, -2147483648, -2147483648",
+    "10, 2147483647, 2147483647"
+  })
+  public void shouldThrowException_WhenOpenIsCalledWithInvalidArgument(int size, int row, int column) {
+    percolation = new Percolation(size);
+
+    percolation.open(row, column);
   }
 
 }
