9262df5e852088260b26c6b03b374c4ab78fe5f7
Constantine Theodoridis <theodoridisconstantine@gmail.com>
2020-07-29 10:31:03 +0300

    Percolation should have full site when site is open and is connected with an open site in top row

--- src/main/java/constantine/theodoridis/problemsolving/percolation/Percolation.java
+++ src/main/java/constantine/theodoridis/problemsolving/percolation/Percolation.java
@@ -1,33 +1,39 @@
 package constantine.theodoridis.problemsolving.percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 
-  private final boolean[][] sites;
+  private final WeightedQuickUnionUF sites;
+  private final boolean[][] isOpen;
   private int numberOfOpenSites;
 
   public Percolation(int size) {
     if (size <= 0) {
       throw new IllegalArgumentException();
     }
-    sites = new boolean[size][size];
+    sites = new WeightedQuickUnionUF(size * size + 1);
+    isOpen = new boolean[size][size];
   }
 
   public void open(int row, int column) {
     validateIndices(row, column);
     if (!isOpen(row, column)) {
-      sites[row - 1][column - 1] = true;
+      isOpen[row - 1][column - 1] = true;
       numberOfOpenSites++;
+      connectWithTopSite(row, column);
+      connectWithTopNeighbor(row, column);
     }
   }
 
   public boolean isOpen(int row, int column) {
     validateIndices(row, column);
-    return sites[row - 1][column - 1];
+    return isOpen[row - 1][column - 1];
   }
 
   public boolean isFull(int row, int column) {
     validateIndices(row, column);
-    return false;
+    return sites.find(getIndex(row, column)) == sites.find(getSize());
   }
 
   public int numberOfOpenSites() {
@@ -35,9 +41,33 @@ public class Percolation {
   }
 
   private void validateIndices(int row, int column) {
-    if (row <= 0 || row > sites.length || column <= 0 || column > sites.length) {
+    if (row <= 0 || row > isOpen.length || column <= 0 || column > isOpen.length) {
       throw new IllegalArgumentException();
     }
+  }
+
+  private void connectWithTopSite(int row, int column) {
+    if (isInTopRow(row)) {
+      sites.union(getIndex(row, column), getSize());
+    }
+  }
+
+  private void connectWithTopNeighbor(int row, int column) {
+    if (!isInTopRow(row) && isOpen(row - 1, column)) {
+      sites.union(getIndex(row, column), getIndex(row - 1, column));
+    }
+  }
+
+  private int getIndex(int row, int column) {
+    return isOpen.length * (column - 1) + row - 1;
+  }
+
+  private int getSize() {
+    return isOpen.length * isOpen.length;
+  }
+
+  private boolean isInTopRow(int row) {
+    return row == 1;
   }
 
 }
